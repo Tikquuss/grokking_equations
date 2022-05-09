4fa82346f979823c8a1b5a6fb54c3d29938d35c7
Tikquuss <pascalnotsawo@gmail.com>
2022-05-09 16:28:02 -0400

    up to date version of pytorch lightning

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     packages=find_packages(),
     version="0.0.1",
     install_requires=[
-        "pytorch_lightning==1.5",
-        #"torch==1.11.0",
+        "pytorch_lightning",
+        "torch",
         #"torchtext<0.12.0", # to avoid `No module named 'torchtext.legacy'` with `import pytorch_lightning as plt`
-        "torchtext==0.11.2",
+        #"torchtext==0.11.2",
         #"torch==1.10.2", will be automatically installed by torchtext
         "blobfile",
         "numpy",
